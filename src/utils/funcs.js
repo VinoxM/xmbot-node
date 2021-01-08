740dea6830efb6ea526e0a123ce99fe8b70c60a8
VinoxM <31297195+VinoxM@users.noreply.github.com>
2021-01-08 16:33:54 +0800

    update

--- src/utils/funcs.js
+++ src/utils/funcs.js
@@ -55,7 +55,6 @@ export function checkIsPrivate(context) {
 
 export function getWebFile(url, type, needProxy = false) {// 获取网页图片,暂只支持http
     let proxy = global['config']['default'].proxy
-    console.log(proxy)
     return new Promise((resolve, reject) => {
         request({
             url: url,
